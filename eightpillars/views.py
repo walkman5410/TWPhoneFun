660096e7429ae3492328fa7d6d70549a2de7d533
Tim <walkman5410@yahoo.com>
2021-11-20 21:02:45 -0500

    add log

--- eightpillars/views.py
+++ eightpillars/views.py
@@ -367,7 +367,7 @@ def add_eightpillar_data(request):
                 except Exception as e:
                     logger.error(e)
                     status = 500
-                    data['message'] += 'There was an error with ' + ticker.ticker + ' '
+                    data['message'] += 'There was an error with ' + ticker.ticker + ' ' + e + ', '
             data['message'] += 'Upload Successful.'
         else:
             logger.error('Missing the data')
